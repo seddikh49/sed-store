a44e39e77d149e3add88b30b00d0b1a403e57520
seddikh49 <seddikh49@gmail.com>
2025-06-09 12:06:28 +0100

    good

---
+++ backend/controllers/user.js
@@ -0,0 +1,16 @@
+
+import jwt from 'jsonwebtoken'
+
+const admin = (req, res) => {
+    const { email, password } = req.body
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+        const token = jwt.sign({ email, role: "admin" },
+            process.env.JWT_SECRET, { expiresIn: '1h' }
+        )
+        res.json({ token: token })
+    } else {
+        res.json({ msg: 'sorry ,access denied' })
+    }
+}
+
+export default admin
